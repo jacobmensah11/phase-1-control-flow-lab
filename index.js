bbc0921d2c7a4013f6a604fdc53784b9eeb9c7fe
jacobmensah11 <jacob.mensah@student.moringaschool.com>
2022-05-12 14:30:46 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance===199){
+    return'This one is on me';
+  } else if(distance ===250){
+    return 'Kindly pay 100 cedis'
+  } else{ return 'Sorry you have to the full amount'
+  }
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  const distance = 199;
+  let responds;
+
+distance >= 200 ? (responds = 'Thank you very much') : (responds = 'Kindly give me a discount');
+
+isAdult;
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  const distance = 199;
+
+let responds;
+
+switch (distance) {
+    case 199:
+        responds = 'Thanks for the tip';
+        break;
+    case 250:
+        responds = 'Thanks for the tip you are giving me ';
+        break;
+    default:
+        console.log("You are welcome");
+        break;
+}
 }
